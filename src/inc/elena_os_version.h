f8d48b083d944c8a496a604c37415782b98ae038
Sab1e-dev <sab1e@qq.com>
2026-01-13 02:15:52 +0800

    chore(release): 升级版本至 v0.1.0-alpha.1

--- src/inc/elena_os_version.h
+++ src/inc/elena_os_version.h
@@ -1,6 +1,8 @@
 /**
  * @file elena_os_version.h
  * @brief 版本定义
+ *
+ * 遵循[Semantic Versioning](https://semver.org/spec/v2.0.0.html)
  * @author Sab1e
  * @date 2025-08-21
  */
@@ -17,10 +19,11 @@ extern "C" {
 #include <stdbool.h>
 
 /* Public macros ----------------------------------------------*/
+
 #define ELENA_OS_VERSION_MAJOR 0
-#define ELENA_OS_VERSION_MINOR 0
+#define ELENA_OS_VERSION_MINOR 1
 #define ELENA_OS_VERSION_PATCH 0
-#define ELENA_OS_VERSION_INFO "alpha"
+#define ELENA_OS_VERSION_INFO "alpha.1"
 
 #define STRINGIFY(x) #x
 #define VERSION_STRING(major, minor, patch, info) STRINGIFY(major) "." STRINGIFY(minor) "." STRINGIFY(patch) "-" info
